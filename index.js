052805f81597bd94cfbd896fde71aff209350ec5
moltak <moltak@gmail.com>
2017-03-11 21:02:27 +0000

    read file info
    what happened
    
    [ticket: X]

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
-const img = require('imagemagick');
+const easyimage = require('easyimage');
 const fs = require('fs');
+const Promise = require('bluebird');
+const rx = require('rxjs');
 
-fs.readdir('src', (err, files) => {
-  files.forEach(i => console.log(i));
-});
+const readDir = Promise.promisify(fs.readdir);
+readDir('src')
+  .then(files => {
+    const promises = [];
+    files.forEach(i => promises.push(easyimage.info(`src/${i}`)));
+    rx
+      .Observable
+      .forkJoin(promises)
+      .subscribe(i => console.log(i));
+  })
+  .catch(err => console.log(err));
