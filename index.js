1700c4afa7baf3bfed5f032872481c2541ae519d
moltak <moltak@gmail.com>
2017-03-11 21:42:27 +0000

    완료!
    what happened
    
    [ticket: X]

--- index.js
+++ index.js
@@ -8,9 +8,22 @@ readDir('src')
   .then(files => {
     const promises = [];
     files.forEach(i => promises.push(easyimage.info(`src/${i}`)));
+
     rx
       .Observable
       .forkJoin(promises)
+      .flatMap(i => [].concat.apply([], i))
+      .map(i => {
+        const opt = {
+          src: i.path, 
+          dst: `dst/${i.name}`,
+          cropwidth: i.width - 2,
+          cropheight: i.height,
+          x: 0,
+        };
+        return easyimage.crop(opt).then();
+      })
+      .flatMap(i => i)
       .subscribe(i => console.log(i));
   })
   .catch(err => console.log(err));
